8e873ea12c6ee04e7c644059bc03767df79d11e1
CrypticMessenger <2020csb1072@iitrpr.ac.in>
2023-01-31 18:51:53 +0530

    added user authentication 👤

--- app/db/db.sql
+++ app/db/db.sql
@@ -3,7 +3,7 @@
 -- TODO: check if pre_req is also a course or not. [course catalog].
 -- TODO: refractor code to maybe separate different modules or even triggers and functions.
 -- TODO: Is it better to go with individual table for each student or same table for all students, for different semester_AY table.
-
+-- TODO: add semester and AY
 --!-----------------------------------------------* course_catalog starts *----------------------------------------------------------------
 drop table course_catalog;
 create table course_catalog(
@@ -38,27 +38,47 @@ for each row
 execute procedure add_credit();
 --------------------------------------------------* course_catalog ends *----------------------------------------------------------------
 
---!-----------------------------------------------* Student authentication starts *----------------------------------------------------------------
-drop table student_auth;
-create table student_auth (
+--!-----------------------------------------------* User authentication starts *----------------------------------------------------------------
+drop table user_auth;
+create table user_auth (
     name varchar(255) not null,
     id varchar(255) not null,
     pwd varchar(255) not null,
+    roles varchar(255) not null,
     primary key (id)
 );
 
+CREATE OR REPLACE FUNCTION add_student_record()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+declare
+sem integer;
+BEGIN
+    If new.roles = 's' then 
+    EXECUTE 'create table if not exists '
+      || quote_ident('s' || substring(new.id,1,11))
+      || ' (
+        sem integer,
+        AY text,
+        courses text [],
+        grades text [],
+        cgpa numeric(10,2),
+        credits_earned integer default 0
+      )';
+    end if;
+	
+	RETURN new;
+END;
+$$;
 
---------------------------------------------------* Student Authentication ends *----------------------------------------------------------------
-
---!-----------------------------------------------* Prof authentication starts *----------------------------------------------------------------
-drop table prof_auth;
-create table prof_auth (
-    id varchar(255) not null,
-    name varchar(255) not null,
-    pwd varchar(255) not null,
-    primary key (id)
-);
---------------------------------------------------* Prof Authentication ends *----------------------------------------------------------------
+create trigger create_student_record 
+after insert 
+on user_auth
+for each row 
+execute procedure add_student_record();
+--------------------------------------------------* User Authentication ends *----------------------------------------------------------------
 
 --!-----------------------------------------------* course_offerings starts *----------------------------------------------------------------
 drop table course_offerings;
@@ -69,9 +89,34 @@ create table course_offerings(
     cg_constraint numeric(10,3) default 0,
     primary key (id),
     constraint check_course foreign key (course_code) references course_catalog(course_code),
-    constraint check_inst foreign key (instructor_id) references prof_auth(id)
+    constraint check_inst foreign key (instructor_id) references user_auth(id)
 );
 --------------------------------------------------* course_offerings ends *----------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION login_check(username text, password text)
+RETURNS text AS $$
+DECLARE
+    result text;
+BEGIN
+    SELECT INTO result
+        CASE
+            WHEN (SELECT count(*) FROM user_auth WHERE id = login_check.username AND pwd = login_check.password and roles = 's') > 0 THEN 's'
+            WHEN (SELECT count(*) FROM user_auth WHERE id = login_check.username AND pwd = login_check.password and roles = 'p') > 0 THEN 'p'
+            WHEN (SELECT count(*) FROM user_auth WHERE id = login_check.username AND pwd = login_check.password and roles = 'a') > 0 THEN 'a'
+            ELSE 'f'
+        END;
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+
+
+
+
+
 
 
 
@@ -82,13 +127,27 @@ insert into course_catalog(course_code, L, T,P,pre_req) values('CS302',3,1,0, Ar
 insert into course_catalog(course_code, L, T,P) values('CS303',3,1,2);
 insert into course_catalog(course_code, L, T,P,pre_req) values('CS304',3,1,2, array ['CS303']);
 insert into course_catalog(course_code, L, T,P) values('CS305',3,0,2);
-insert into student_auth(id,name,pwd) values('2020csb1070@iitrpr.ac.in','Amit Kumar','X123');
-insert into student_auth(id,name,pwd) values('2020csb1072@iitrpr.ac.in','Ankit Sharma','X123');
-insert into student_auth(id,name,pwd) values('2020csb1074@iitrpr.ac.in','Arshdeep Singh','X123');
-insert into prof_auth(id,name,pwd) values('apurva@iitrpr.ac.in','Apurva Mudgal','X123');
-insert into prof_auth(id,name,pwd) values('gunturi@iitrpr.ac.in','V. Gunturi','X123');
-insert into prof_auth(id,name,pwd) values('balwinder@iitrpr.ac.in','Balwinder Sodhi','X123');
+insert into user_auth(id,name,pwd,roles) values('2020csb1070@iitrpr.ac.in','Amit Kumar','X123','s');
+insert into user_auth(id,name,pwd,roles) values('2020csb1072@iitrpr.ac.in','Ankit Sharma','X123','s');
+insert into user_auth(id,name,pwd,roles) values('2020csb1074@iitrpr.ac.in','Arshdeep Singh','X123','s');
+insert into s2020csb1070(sem,ay) values(2,'2020-2021');
+insert into s2020csb1070(sem,ay) values(1,'2021-2022');
+insert into s2020csb1070(sem,ay) values(2,'2021-2022');
+insert into s2020csb1070(sem,ay) values(1,'2022-2023');
+insert into s2020csb1070(sem,ay) values(2,'2022-2023');
+insert into s2020csb1070(sem,ay) values(1,'2023-2024');
 
+insert into s2020csb1072(sem,ay) values(2,'2020-2021');
+insert into s2020csb1072(sem,ay) values(1,'2021-2022');
+insert into s2020csb1072(sem,ay) values(2,'2021-2022');
+insert into s2020csb1072(sem,ay) values(1,'2022-2023');
+insert into s2020csb1072(sem,ay) values(2,'2022-2023');
+insert into s2020csb1072(sem,ay) values(1,'2023-2024');
+
+insert into user_auth(id,name,pwd,roles) values('apurva@iitrpr.ac.in','Apurva Mudgal','X123','p');
+insert into user_auth(id,name,pwd,roles) values('gunturi@iitrpr.ac.in','V. Gunturi','X123','p');
+insert into user_auth(id,name,pwd,roles) values('balwinder@iitrpr.ac.in','Balwinder Sodhi','X123','p');
+insert into user_auth(id,name,pwd,roles) values('admin@iitrpr.ac.in','Admin','X123','a');
 insert into course_offerings(course_code, instructor_id) values('CS302','apurva@iitrpr.ac.in');
 insert into course_offerings(course_code, instructor_id) values('CS301','gunturi@iitrpr.ac.in');
 insert into course_offerings(course_code, instructor_id,cg_constraint) values('CS305','balwinder@iitrpr.ac.in',7.5);
--- app/src/main/java/studentmanagement/Client.java
+++ app/src/main/java/studentmanagement/Client.java
@@ -1,19 +1,97 @@
 package studentmanagement;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Client {
+    private final String url = "jdbc:postgresql://localhost/academic_management";
+    private final String user = "postgres";
+    private final String password = "1421";
+    private String current_user = "";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public Boolean getResultSet(Connection conn, String query) {
+        Boolean flag = false;
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1)
+                    System.out.print("  ");
+                System.out.print(rsmd.getColumnName(i));
+            }
+            System.out.println("");
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1)
+                        System.out.print("  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue + " ");
+                }
+                System.out.println("");
+                if (!flag) {
+                    flag = true;
+                }
+            }
+            return flag;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return flag;
+        }
+
+    }
 
     public static void main(String[] args) {
+        Client app = new Client();
+        Connection conn = app.connect();
         // make an authetication system for users
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your username: ");
-        String username = sc.nextLine();
+        System.out.println("Enter your email id: ");
+        String id = sc.nextLine();
         System.out.println("Enter your password: ");
-        String password = sc.nextLine();
+        String pwd = sc.nextLine();
 
-        System.out.println(username);
-        System.out.println(password);
+        // TODO: deal with SQL injection attacks
+        try {
+            Statement st = conn.createStatement();
+            System.out.println("SELECT login_check(" + id + "," + pwd + ")");
+            ResultSet rs = st.executeQuery("SELECT login_check('" + id + "','" + pwd + "')");
+            rs.next();
+            String result = rs.getString(1);
+            if (result.equals("s")) {
+                System.out.println("Welcome student!");
+            } else if (result.equals("p")) {
+                System.out.println("Welcome professor!");
+            } else if (result.equals("a")) {
+                System.out.println("Welcome admin!");
+            } else if (result.equals("f")) {
+                System.out.println("Login failed!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(id);
+        System.out.println(pwd);
         sc.close();
 
     }
